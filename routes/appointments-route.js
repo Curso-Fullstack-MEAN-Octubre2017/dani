0ce976c9b8e2bae8632ef5a923b6828aba6f4ee3
JAVA <JAVA@user9a4>
2017-10-19 11:59:27 +0200

    Task: #1: Persistencia + API: Leer Lista Clientes

--- routes/appointments-route.js
+++ routes/appointments-route.js
@@ -1,5 +1,8 @@
 const Appointment = require('../models/appointments-model');
+const Pet = require('../models/pets-model');
+const Customer = require('../models/customers-model');
 var moment = require('moment');
+
 
 module.exports = (router) => {
 	
@@ -31,14 +34,51 @@ module.exports = (router) => {
 	router.get('/appointments/:fromdate/:todate', function(req, res){
 		
 		var fechaInicio = moment(req.params.fromdate, 'YYYYMM');
-		var fecahFin = moment(req.params.todate, 'YYYYMM');
+		var fechaFin = moment(req.params.todate, 'YYYYMM');
 		
 		Appointment.find({dateStart : {"$gte": fechaInicio, "$lt": fechaFin}}, function(err, appointments){
 			if (err) {
 				console.error(err);
 				res.sendStatus(500);
 			} else {
-				res.json(appointments);
+				Appointment.populate(appointments, {path: "petId", populate: {path: "ownerId"}}, function(err, appointments){
+					res.json(appointments);
+				});
+			}
+		});
+	});
+	
+	//Recuperamos una cita a través de la fecha de inicio y la fecha de fin:
+	router.get('/appointments/:fromdate/:todate', function(req, res){
+		
+		var fechaInicio = moment(req.params.fromdate, 'YYYYMM');
+		var fechaFin = moment(req.params.todate, 'YYYYMM');
+		
+		Appointment.find({dateStart : {"$gte": fechaInicio, "$lt": fechaFin}}, function(err, appointments){
+			if (err) {
+				console.error(err);
+				res.sendStatus(500);
+			} else {
+				Appointment.populate(appointments, {path: "petId", populate: {path: "ownerId"}}, function(err, appointments){
+					res.json(appointments);
+				});
+			}
+		});
+	});
+		
+	//Recuperamos una cita a través de la fecha de inicio y la fecha de fin y filtramos los datos:
+	router.get('/appointments/:month', function(req, res){
+		
+		var mes = moment(req.params.month, 'YYYYMM');
+		
+		Appointment.find({dateStart: mes}, function(err, appointments){
+			if (err) {
+				console.error(err);
+				res.sendStatus(500);
+			} else {
+				Appointment.populate(appointments, {path: "petId", populate: {path: "ownerId"}}, function(err, appointments){
+					res.json(appointments);
+				});
 			}
 		});
 	});
